667d464e42de1e7a24812d742bc4921b3aaa916a
Colin Chauche <colin.chauche@gmail.com>
2021-05-22 09:05:53 -0600

    Add History model

---
+++ models/History.js
@@ -0,0 +1,39 @@
+/**
+ * Class to store and retrieve the history of math operations.
+ * @constructor
+ */
+
+class History {
+  constructor() {
+    this.storage = [];
+  }
+
+  /**
+   *
+   * @param {number} a
+   * @param {number} b
+   * @param {string} operation String should be one of the following: 'add', 'subtract', 'multiply', or 'divide'.
+   * @param {number} answer
+   * @returns {Promise<Object>} A promise that resolves to '1 Record Added OK'
+   */
+  save(a, b, operation, answer) {
+    let entry = {
+      a,
+      b,
+      operation,
+      answer,
+    };
+    this.storage.push(entry);
+    return Promise.resolve('1 Record added OK');
+  }
+
+  /**
+   *
+   * @returns {Promise<Object>} Returns a promise that resolves to an array of the math operation history
+   */
+  getAll() {
+    return Promise.resolve(this.storage);
+  }
+}
+
+module.exports = new History();
